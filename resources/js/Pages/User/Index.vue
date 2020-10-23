9fb1abe1cbdd00fd3f114b46a564f447a2bc9732
Hoon <kimhoon0316@gmail.com>
2020-10-23 15:06:49 +0900

    inertia install!

---
+++ resources/js/Pages/User/Index.vue
@@ -0,0 +1,10 @@
+<template>
+<div>
+    <h1>Hello, {{ name }}, Your Email: {{ email }}</h1>
+</div>
+</template>
+<script>
+export default {
+    props: ['name', 'email']
+}
+</script>
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,33 @@
 require('./bootstrap');
+
+import { App, plugin } from '@inertiajs/inertia-vue';
+import Vue from 'vue';
+import { InertiaProgress } from '@inertiajs/progress';
+
+Vue.use(plugin);
+InertiaProgress.init({
+	// The delay after which the progress bar will
+	// appear during navigation, in milliseconds.
+	delay: 250,
+
+	// The color of the progress bar.
+	color: '#29d',
+
+	// Whether to include the default NProgress styles.
+	includeCSS: true,
+
+	// Whether the NProgress spinner will be shown.
+	showSpinner: false
+});
+
+const el = document.getElementById('app');
+
+new Vue({
+	render: (h) =>
+		h(App, {
+			props: {
+				initialPage: JSON.parse(el.dataset.page),
+				resolveComponent: (name) => require(`./Pages/${name}`).default
+			}
+		})
+}).$mount(el);
